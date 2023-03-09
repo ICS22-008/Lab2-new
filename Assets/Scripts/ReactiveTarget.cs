a396a648d0225aeed8dab7ad18b95efeb788eabf
ICS22-008 <113048061+ICS22-008@users.noreply.github.com>
2023-03-09 13:55:03 -0800

    finished task 1 lab 06

--- Assets/Scripts/ReactiveTarget.cs
+++ Assets/Scripts/ReactiveTarget.cs
@@ -12,12 +12,25 @@ public class ReactiveTarget : MonoBehaviour
         {
             enemyAI.ChangeState(EnemyStates.dead);
         }
-        StartCoroutine(Die());
+        Animator enemyAnimator = GetComponent<Animator>();
+        if (enemyAnimator != null)
+        {
+            Debug.Log("In the animator");
+            enemyAnimator.SetTrigger("Die");
+        }
+        //StartCoroutine(Die());
+        
     }
 
+    private void DeadEvent()
+    {
+        Destroy(this.gameObject);
+    }
     private IEnumerator Die() {
-        iTween.RotateAdd(this.gameObject, new Vector3(-75, 0, 0), 1);
+        //iTween.RotateAdd(this.gameObject, new Vector3(-75, 0, 0), 1);
         yield return new WaitForSeconds(2);
         Destroy(this.gameObject);
     }
+
+    
 }
