3bf8d98d1470201f9b071b98903387735dea78d6
ICS22-008 <113048061+ICS22-008@users.noreply.github.com>
2023-03-10 14:03:30 -0800

    Finished lab07

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,6 +6,11 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField]
     private GameObject enemyPrefab;
+    [SerializeField]
+    private GameObject iguanaPrefab;
+    private GameObject[] iguanas;
+    private int numIguanas = 5;
+    private Vector3 spawnPointIguana = new Vector3(9.74f, 0, -4.54f);
     //private GameObject enemy;
     private Vector3 spawnPoint = new Vector3(0, 0, 5);
     private int numEnemiesToSpawn = 5;
@@ -13,7 +18,17 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        enemies = new GameObject[numEnemiesToSpawn];    
+        enemies = new GameObject[numEnemiesToSpawn];
+        iguanas = new GameObject[numIguanas];
+        for (int i = 0; i < iguanas.Length; i++) {
+            if (iguanas[i] == null) {
+                iguanas[i] = Instantiate(iguanaPrefab) as GameObject;
+                iguanas[i].transform.position = spawnPointIguana;
+                float angle = Random.Range(0, 360);
+                iguanas[i].transform.Rotate(0, angle, 0);
+            }
+            
+        }
     }
 
     // Update is called once per frame
